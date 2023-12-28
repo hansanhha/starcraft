974253c2a8fb5fe7102db115975a6334ef3a0cac
hansanhha <kwixs35@gmail.com>
2023-12-29 00:01:22 +0900

    first commit

---
+++ src/main/java/hansanhha/spring/bootcore/BootcoreApplication.java
@@ -0,0 +1,13 @@
+package hansanhha.spring.bootcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hansanhha/spring/bootcore/BootcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hansanhha.spring.bootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
