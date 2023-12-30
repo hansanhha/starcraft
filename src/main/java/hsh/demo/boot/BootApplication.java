7a812599afec88892cede7716a99fbcc75cd69ba
hansanhha <kwixs35@gmail.com>
2023-12-30 21:47:15 +0900

    커스텀 배너 추가

--- src/main/java/hsh/demo/boot/BootApplication.java
+++ src/main/java/hsh/demo/boot/BootApplication.java
@@ -1,5 +1,6 @@
 package hsh.demo.boot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
