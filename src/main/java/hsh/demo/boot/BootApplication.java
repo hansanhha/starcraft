16a9d7343f70b242d398c548303c442875a0e5a8
hansanhha <kwixs35@gmail.com>
2024-01-01 19:32:16 +0900

    Spring Application Startup tracking 추가
    [공식문서](https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.spring-application.application-events-and-listeners) 참고 필요

--- src/main/java/hsh/demo/boot/BootApplication.java
+++ src/main/java/hsh/demo/boot/BootApplication.java
@@ -3,12 +3,15 @@ package hsh.demo.boot;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+        SpringApplication application = new SpringApplication(BootApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
     }
 
 }
