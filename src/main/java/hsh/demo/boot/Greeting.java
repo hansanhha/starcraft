bae1a356534e46de53f741a23b0a417208438d6b
hansanhha <kwixs35@gmail.com>
2024-01-02 20:14:56 +0900

    PropertySource 코드 추가

---
+++ src/main/java/hsh/demo/boot/Greeting.java
@@ -0,0 +1,47 @@
+package hsh.demo.boot;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/*
+    * PropertySource 사용(@Value)
+    * 프로퍼티 파일
+    * OS 환경변수
+    * 시스템 속성
+    * SPRING_APPLICATION_JSON
+    * CLI 인자
+    * Property Placeholders 기본값
+ */
+@Component
+public class Greeting {
+
+    @Value("${message.hello:placeholder default hello message}")
+    private String helloMessage;
+
+    @Value("${message.bye:placeholder default bye message}")
+    private String byeMessage;
+
+    @Value("${message.home:placeholder default home message}")
+    private String homeMessage;
+
+    @PostConstruct
+    public void init() {
+        System.out.println();
+        sayHello();
+        sayHome();
+        sayBye();
+    }
+
+    public void sayHello() {
+        System.out.println(helloMessage);
+    }
+
+    public void sayBye() {
+        System.out.println(byeMessage);
+    }
+
+    public void sayHome() {
+        System.out.println(homeMessage);
+    }
+}
